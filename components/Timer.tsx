316ed29c26af743730b9ab285a0be8f54e61d54a
tmz81 <tmz_81@hotmail.com>
2022-08-03 15:43:18 -0300

    create component Timer

---
+++ components/Timer.tsx
@@ -0,0 +1,28 @@
+import styles from '../styles/Timer.module.css'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+
+interface TimerProps {
+  key: any;
+  duration: number;
+  timesUp: () => void;
+}
+
+export default function Timer(props: TimerProps) {
+  return (
+    <div className={styles.timer}>
+      <CountdownCircleTimer
+        duration={props.duration}
+        size={150}
+        onComplete={props.timesUp}
+        colors={[
+          ['#BCE596', 0.33],
+          ['#F7B801', 0.33],
+          ['#ED827A', 0.33],
+        ]}
+        isPlaying
+      >
+        {({ remainingTime }) => remainingTime}
+      </CountdownCircleTimer>
+    </div>
+  )
+}
